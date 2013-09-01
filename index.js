fb6b3ccdd0d4e1a106a4c5ec902575295647fcc2
Alex Cordonnier <alexjcord@gmail.com>
2013-08-31 22:39:36 -0500

    Fixed more bugs

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 //Toggle the dropdowns when clicked
 $('.expand').click(function() {
   var info = $(this).parent().next();
+  //Change the plus/minus icon
   if (info.css('display') == 'none') {
     $(this).text('[-]');
   } else {
     $(this).text('[+]');
   }
+  //Animate the toggle with a sliding animation
   info.slideToggle(200);
 });
 
+//Generate the validator link based on the current URL
 $('#valid').attr('href', 'http://validator.w3.org/check?uri=' +
     encodeURIComponent(location.href));
 
+//Make the print link actually print the page
 $('#print-link').click(function(){print()});
