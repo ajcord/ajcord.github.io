6135fe579379e196ccbed6a51a6aadf29be678e0
Alex Cordonnier <alexjcord@gmail.com>
2013-09-02 17:24:44 -0500

    Added page load animation & better GA tracking
    Also fixed section margins in print stylesheet

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
+//Stores whether the page is being printed via the in-page link
+var printingFromLink = false;
+//Stores the time of the last print event to prevent multiple GA events from
+//being fired.
+var lastPrintEvent = 0;
+
 //Toggle the dropdowns when clicked
 $('.expand').click(function() {
   var info = $(this).parent().next();
-  //Change the plus/minus icon
+  //Change the plus/minus icon and track the event
   if (info.css('display') == 'none') {
     $(this).text('[-]');
+    //Track the expansion of the section
+    _gaq.push(['_trackEvent', $(this).parent().parent().attr('id'), 'expanded',
+        $(this).parent().attr('id')]);
   } else {
     $(this).text('[+]');
+    //Track the collapse of the section
+    _gaq.push(['_trackEvent', $(this).parent().parent().attr('id'), 'collapsed',
+        $(this).parent().attr('id')]);
   }
   //Animate the toggle with a sliding animation
   info.slideToggle(200);
@@ -16,4 +28,52 @@ $('#valid').attr('href', 'http://validator.w3.org/check?uri=' +
     encodeURIComponent(location.href));
 
 //Make the print link actually print the page
-$('#print-link').click(function(){print()});
+$('#print-link').click(function(){
+  printingFromLink = true;
+  window.print();
+});
+
+//Track downloads of the PDF version of the resume
+$('#download-link').click(function() {
+  _gaq.push(['_trackEvent', 'resume', 'downloaded', 'PDF']);
+});
+
+//Track clicks on all outbound links
+$('a[href^=http]').click(function() {
+  _gaq.push(['_trackEvent', 'links', 'clicked',
+      $(this).text() + ' - ' + $(this).attr('href')]);
+});
+
+
+
+(function() {
+  var afterPrint = function() {
+    //Track the number of times users print the resume
+    var currentTime = new Date().getTime();
+    //Chrome fires the event twice for some reason. I only want it to happen
+    //once, so I am excluding rapid events.
+    if (currentTime - lastPrintEvent > 1000) {
+      lastPrintEvent = currentTime;
+      var label = '';
+      if (printingFromLink) {
+        label = 'via link';
+      } else {
+        label = 'via browser';
+      }
+      _gaq.push(['_trackEvent', 'resume', 'print', label]);
+      printingFromLink = false;
+    }
+  };
+  
+  //The following code is necessary to get onafterprint working cross-browser.
+  //See http://tjvantoll.com/2012/06/15/detecting-print-requests-with-javascript
+  if (window.matchMedia) {
+    var mediaQueryList = window.matchMedia('print');
+    mediaQueryList.addListener(function(mql) {
+      if (!mql.matches) {
+        afterPrint();
+      }
+    });
+  }
+  window.onafterprint = afterPrint;
+}());
