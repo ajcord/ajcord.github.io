1f7b97e48b7f640008faae081efb4f0bb301269e
Alex Cordonnier <alexjcord@gmail.com>
2019-03-17 17:16:30 -0700

    Use new LaTeX resume, remove print feature, fix HTML5 compliance, and minor content updates

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-//Stores whether the page is being printed via the in-page link
-var printingFromLink = false;
-//Stores the time of the last print event to prevent multiple GA events from
-//being fired.
-var lastPrintEvent = 0;
-
 //Toggle the dropdowns when clicked
 $(".expand").click(function() {
   var info = $("." + this.parentNode.id.replace("-dropdown", "-info"));
@@ -30,12 +24,6 @@ $(".expand").click(function() {
 $("#valid").attr("href", "http://validator.w3.org/check?uri=" +
     encodeURIComponent(location.href));
 
-//Make the print link actually print the page
-$("#print-link").click(function(){
-  printingFromLink = true;
-  window.print();
-});
-
 //Track downloads of the PDF version of the resume
 $("#download-link").click(function() {
   ga("send", "event", "resume", "downloaded", "PDF");
@@ -46,40 +34,6 @@ $("a[href^=http]").click(function() {
   ga("send", "event", "links", "clicked",
       $(this).text() + " - " + $(this).attr("href"));
 });
-
-
-//Set up print logging
-(function() {
-  var afterPrint = function() {
-    //Track the number of times users print the resume
-    var currentTime = new Date().getTime();
-    //Chrome fires the event twice for some reason. I only want it to happen
-    //once, so I am excluding rapid events.
-    if (currentTime - lastPrintEvent > 1000) {
-      lastPrintEvent = currentTime;
-      var label = "";
-      if (printingFromLink) {
-        label = "via link";
-      } else {
-        label = "via browser";
-      }
-      ga("send", "event", "resume", "printed", label);
-      printingFromLink = false;
-    }
-  };
-  
-  //The following code is necessary to get onafterprint working cross-browser.
-  //See http://tjvantoll.com/2012/06/15/detecting-print-requests-with-javascript
-  if (window.matchMedia) {
-    var mediaQueryList = window.matchMedia("print");
-    mediaQueryList.addListener(function(mql) {
-      if (!mql.matches) {
-        afterPrint();
-      }
-    });
-  }
-  window.onafterprint = afterPrint;
-}());
 
 
 //Set up minimap if transforms are supported
